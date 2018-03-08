a3bc0de2894c03187f7eb24ed5466077bfd703d7
Laboratorio de Computación T-204 <t204@laburl>
2018-03-08 08:23:59 -0600

    Firs Commit

---
+++ Lab4/Controllers/PorFechaController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab4.Controllers
+{
+    public class PorFechaController : Controller
+    {
+        //
+        // GET: /PorFecha/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /PorFecha/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /PorFecha/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /PorFecha/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /PorFecha/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /PorFecha/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /PorFecha/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /PorFecha/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Lab4/Models/ArbolAVL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab4.Models
+{
+    public class ArbolAVL
+    {
+        public int NoPartido { get; set; }
+        public DateTime FechaPartido { get; set; }
+        public string Grupo { get; set; }
+        public string Pais1 { get; set; }
+        public string Pais2 { get; set; }
+        public string Estadio { get; set; }
+ 
+
+
+    }
+}
